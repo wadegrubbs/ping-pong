e089bf0f7ac379746068fce9be881fccdfbdb3ef
Wade Grubbs <wadegrubbs@gmail.com>
2016-01-22 09:19:31 -0800

    test if input is divisible by 3 and not 15

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,17 @@ describe ('pingPong', function () {
  it("is not valid numerical input", function () {
    expect(pingPong("words")).to.equal("true")
  });
+
+ it("is divisible by 3 and not 15", function () {
+   expect(pingPong("3")).to.equal("true")
+ });
+
+ // it("is not valid numerical input", function () {
+ //   expect(pingPong("words")).to.equal("true")
+ // });
+ //
+ // it("is not valid numerical input", function () {
+ //   expect(pingPong("words")).to.equal("true")
+ // });
+
  });

531ba6e65dd5caea9a498f1256644cfadbfd7a1e
Wade Grubbs <wadegrubbs@gmail.com>
2016-01-22 14:10:17 -0800

    add js and html form functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 var pingPong = function ( number ) {
 
-  // if (number = isNaN(number)) {
+var number;
+  // if (number.isNaN) {
   //   return "true";
   // }
+
   if ((number % 3 === 0) && (number % 15 !== 0)) {
+    // console.log(pingPong(number))
     return "ping";
   }
+
   else if ((number % 5 === 0) && (number % 15 !== 0)) {
     return "pong";
   }
@@ -14,14 +18,31 @@ var pingPong = function ( number ) {
     return "pingpong";
   }
 
-
-
+  else {
+    return number;
+  }
 
 }
 
-
-
-
-
+var number = pingPong(number)
 $(document).ready(function() {
+  $("form#pingPongForm").submit(function(event) {
+    var number = $("input#inputNumber").val();
+    var result = pingPong(number);
+    console.log(result);
+    $(".output").text;
+
+    console.log(number);
+    console.log(result);
+
+    $("#result").show().append("<p>This is " + result + "</p>");
+    $("#result").show();
+    event.preventDefault();
+  })
 })
+
+
+
+
+// $(document).ready(function() {
+// })
